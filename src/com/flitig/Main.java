0dcd89d2d71fadc8b42ce8a0f099cc6da11599db
Flitig <flitig@kth.se>
2016-06-13 21:01:37 +0200

    reading input

--- src/com/flitig/Main.java
+++ src/com/flitig/Main.java
@@ -1,8 +1,43 @@
 package com.flitig;
 
+import java.io.InputStreamReader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean run = false;
+        int targetVolume = 0;
+        int firstBottle = 0;
+        int secondBottle = 0;
+
+        int[] input;
+        do {
+            System.out.println("Please provide three integers: target volume and the capacity of two bottles. Example: 1 5 3 \nInput anything else to quit.");
+            Scanner sc = new Scanner(System.in);
+
+            try {
+                targetVolume = sc.nextInt();
+                firstBottle = sc.nextInt();
+                secondBottle = sc.nextInt();
+            } catch (InputMismatchException e) {
+                exit(0);
+            }
+            finally {
+                sc.close();
+            }
+
+
+            input = new int[]{targetVolume, firstBottle, secondBottle};
+            System.out.println(BottleExercise(input));
+
+        } while (run);
+    }
+
+    private static String BottleExercise(int[] args) {
+        return String.format(Integer.toString(args[0]));
     }
 }
